e488fa8dd1b81afdcfb0fa81e206e8ead4f14441
defau1tuser0 <iaditya8424@gmail.com>
2026-02-03 14:26:31 +0530

    Changeable window screen with minimum width and height of 300

--- main.cpp
+++ main.cpp
@@ -1,13 +1,39 @@
 #include <raylib.h>
 #include <iostream>
+#include <string>
 #include "simulation.hpp"
 
 int main() {
-    const int WINDOW_WIDTH = 1200;
-    const int WINDOW_HEIGHT = 800;
-    int CELL_SIZE;
+    std::string window_width;
+    std::string window_height;
+    std::cout << "Enter the width of simulation screen(press Enter for default 1200): ";
+    std::getline(std::cin, window_width);
+    std::cout << "Enter the height of simulation screen(press Enter for default 800): ";
+    std::getline(std::cin, window_height);
+
+    int new_window_width;
+    int new_window_height;
+    //if input is empyt(Enter key) set the value to default
+    if (window_width.empty() || window_height.empty()) {
+        new_window_width = 1200;
+        new_window_height = 800;
+    }
+    else { //else change the strong to int
+        new_window_width = std::stoi(window_width);
+        new_window_height = std::stoi(window_height);
+    }
+    
+    
+    //minimum size should be 500
+    const int WINDOW_WIDTH = (new_window_width<300)? 300 : new_window_width;
+    const int WINDOW_HEIGHT = (new_window_height<300)? 300 : new_window_height;
+    std::cout << "Window Width: " << WINDOW_WIDTH << std::endl;
+    std::cout << "Window Height: " << WINDOW_HEIGHT << std::endl;
+
+    int cell_size;
     std::cout << "Enter the size of cells: ";
-    std::cin >> CELL_SIZE;
+    std::cin >> cell_size;
+    const int CELL_SIZE = (cell_size<=1)? 2 : cell_size;
     int cell_count;
     std::cout << "Cell Size: " << CELL_SIZE << std::endl;
     std::cout << "Cell Count: " << int(WINDOW_HEIGHT * WINDOW_WIDTH) / CELL_SIZE << std::endl;
